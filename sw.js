4c198f4f127d19a0cfd4af0bf7e973d66b36103f
cunil17 <sajojo237@gmail.com>
2026-02-05 12:45:05 +0700

    Update service worker cache and assets management

--- sw.js
+++ sw.js
@@ -1,15 +1,30 @@
-const CACHE = "metronome-v1";
+const CACHE_NAME = "metronome-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icons-192.png",
+  "./icons/icons-512.png"
+];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "/SimpleMetronome/",
-        "/SimpleMetronome/index.html",
-        "/SimpleMetronome/manifest.json"
-      ])
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
